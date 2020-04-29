75353f9a2a5ce276ae5b274f5c4ba2ee3befc34a
parkoon <devparkoon@gmail.com>
2020-04-29 12:35:31 +0900

    add view engine and scss

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
 import express from 'express'
+import path from 'path'
+import exphbs from 'express-handlebars'
 
 const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.engine(
+  'hbs',
+  exphbs({
+    extname: 'hbs',
+  })
+)
+app.set('view engine', 'hbs')
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
 
 export default app
