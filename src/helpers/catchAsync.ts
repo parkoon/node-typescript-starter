0cded3ff2ccc2f97a59be5585b222dddae9856c1
parkoon <devparkoon@gmail.com>
2020-05-06 15:58:22 +0900

    add catch async helper

---
+++ src/helpers/catchAsync.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { RequestWithUser } from '../interfaces/http.interface';
+import AppException from '../exceptions/app.exception';
+
+/**
+ * async / await 의 try catch 반복 작업을 줄여주는 헬퍼
+ * 에러 발생했을 경우 에러 미들웨어로 전달
+ */
+export default (fn: Function) => {
+  return (req: Request | RequestWithUser, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch((err: AppException) => next(err));
+  };
+};
