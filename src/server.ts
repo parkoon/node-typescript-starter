f362862b1850b77ab5a23224c279dfbd71b578f3
parwhdgur <jh.park@qualson.com>
2020-10-05 15:35:24 +0900

    chore: update console

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const server = app.listen(app.get('port'), () => {
 
 // promise 에서 catch로 에러 처리를 하지 않았을 때 방생하는 요류
 process.on('unhandledRejection', () => {
-  console.log('UNHANDLED REJECTION: Shutting down....!');
+  console.log('UNHANDLED REJECTION: Shutting down!');
   server.close(() => {
     process.exit(1);
   });
@@ -21,7 +21,7 @@ process.on('uncaughtException', () => {
 });
 
 process.on('SIGTERM', () => {
-  console.log('SIGTERM RECEIVED, Shutting down gracefully!!');
+  console.log('SIGTERM RECEIVED, Shutting down gracefully!');
   server.close(() => {
     console.log('Process terminated!');
   });
